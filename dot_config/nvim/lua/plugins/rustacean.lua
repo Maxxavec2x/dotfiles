2f0f04cbb7e2eece6446b0bec43180b9c14c8029
Maxxavec2x <maxencesibonibergard@gmail.com>
2025-12-06 11:57:29 +0100

    maj dotfiles

--- dot_config/nvim/lua/plugins/rustacean.lua
+++ dot_config/nvim/lua/plugins/rustacean.lua
@@ -1,6 +1,15 @@
 -- Plugin pour ajouter des infos liés au rust
 return {
   "mrcjkb/rustaceanvim",
-  version = "^6", -- Recommended
-  lazy = false, -- This plugin is already lazy
+  version = "^6",
+  lazy = false,
+  config = function()
+    vim.g.rustaceanvim = {
+      server = {
+        on_attach = function(client, bufnr)
+          client.server_capabilities.inlayHintProvider = false
+        end,
+      },
+    }
+  end,
 }
